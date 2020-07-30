8b673f0f51fe06c02b7d8cb2b288813d4dab4a1e
Jackson Beytebiere <jacksongrowson@gmail.com>
2020-07-30 22:05:05 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,29 @@
 def addition(num1, num2)
-
+ans=num1+num2
+ans
 end
 
 def subtraction(num1, num2)
-
+ans=num1-num2
+ans
 end
 
 def division(num1, num2)
-
+ans=num1/num2
+ans
 end
 
 def multiplication(num1, num2)
-
+ans=num1*num2
+ans
 end
 
 def modulo(num1, num2)
-
+ans=num1%num2
+ans
 end
 
 def square_root(num)
-
+ans=Math.sqrt(num)
+ans
 end
